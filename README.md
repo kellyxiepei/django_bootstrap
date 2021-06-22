02570300729559f2f32d979ff252bc57deb0c164
xiewangyi <xiewangyi@baicizhan.com>
2021-06-23 00:03:34 +0800

    ignore idea

--- README.md
+++ README.md
@@ -1,6 +1,16 @@
 # django_bootstrap
 
-# Setup development environment.
+# 1.Install poetry
+```
+pip install poetry
+```
+
+# 2.Build project
+```
+poetry build
+```
+
+# 3.Configure database
 ```
 create database django_bootstrap charset=utf8;
 ```
--- project_tools/create_new_project.sh
+++ project_tools/create_new_project.sh
@@ -1,1 +1,12 @@
-echo 'hello'
+if [ -z "$1" ];then
+    echo "Usage: bash create_new_project.sh <project_name>"
+    exit
+fi
+
+git clone git@github.com:kellyxiepei/django_bootstrap.git
+
+PROJECT_NAME=$(echo $1 | tr '[A-Z]' '[a-z]')
+mv django_bootstrap $PROJECT_NAME
+mv "$PROJECT_NAME/django_bootstrap" "$PROJECT_NAME/$PROJECT_NAME"
+
+rm -r "./$PROJECT_NAME/.git"
