b17c23f80131663ca441a8596ade641d2f741a9d
xiewangyi <xiewangyi@baicizhan.com>
2021-07-06 22:36:41 +0800

    use mysql client in pure python

--- django_bootstrap/settings.py
+++ django_bootstrap/settings.py
@@ -14,6 +14,9 @@ import os
 from dynaconf import settings as dyna_settings
 from pathlib import Path
 
+import pymysql
+pymysql.install_as_MySQLdb()
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
